5cac131f9435d7281bccbe4e197a456177a7cec6
K. Stefan <bossob@me.com>
2024-06-28 13:49:53 +0200

    Initial Commit

---
+++ TestedFunctions/TestedFunctionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestedFunctionsApp.swift
+//  TestedFunctions
+//
+//  Created by Stefan kund on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestedFunctionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
